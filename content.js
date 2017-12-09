8b77fe45f179e306d28152d81b1b2c4b9a13f5ed
William Lemens <william.lemens@gmail.com>
2017-12-09 13:17:28 -0600

    Made Ratings th show up right

--- content.js
+++ content.js
@@ -1,8 +1,10 @@
 var tbl = document.getElementsByClassName('rwd-table')[0];
 // Insert "Rating" to thead
 var th = tbl.tHead.children[0];
-var ratingHead = tr.insertCell(5);
-th.innerHTML = "Rating"; // TODO add in a question field that opens a window describing extension
+var ratingHead = document.createElement('th'); //th.insertCell(5);
+th.insertBefore(ratingHead, th.children[5]);
+ratingHead.setAttribute("scope", "col");
+ratingHead.innerHTML = "Rating"; // TODO add in a question field that opens a window describing extension
 // Add cells to body for ratings
-var tb = tbl.tBodies[0];
-for ()
+//var tb = tbl.tBodies[0];
+//for ()
