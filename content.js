6f9e6f7456ce262d9d0769e7330223c5cd60b687
William Lemens <william.lemens@gmail.com>
2017-12-09 12:46:03 -0600

    Initial commit

---
+++ content.js
@@ -0,0 +1,8 @@
+var tbl = document.getElementsByClassName('rwd-table')[0];
+// Insert "Rating" to thead
+var th = tbl.tHead.children[0];
+var ratingHead = tr.insertCell(5);
+th.innerHTML = "Rating"; // TODO add in a question field that opens a window describing extension
+// Add cells to body for ratings
+var tb = tbl.tBodies[0];
+for ()
