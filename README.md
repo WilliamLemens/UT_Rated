03cc2ffd9b45ebe961e3c99c7b6e9f914bc001c1
William Lemens <william.lemens@gmail.com>
2018-05-28 14:06:36 -0500

    Cleaned up input

--- README.md
+++ README.md
@@ -3,10 +3,10 @@ Most of us UT students spend a lot of time switching back and forth between [Rat
 
 Wouldn't it be grand if you could see RateMyProfessor's info right on the schedule?
 
-Well I can do you one better. UT's been [posting the data](https://utdirect.utexas.edu/ctl/ecis/results/index.WBX) from their Course Instructor Surveys for years now, and those are way more useful imo. Less memes and more reliable.
+Well I can do you one better. UT's been posting the [data from their Course Instructor Surveys](https://utdirect.utexas.edu/ctl/ecis/results/index.WBX) for years now, and those are way more useful. Less memes and more reliable.
 
 ### TODO
 - Chrome
-	- Figure out how to scrape info off CIS site.
+	- Wait for the CIS team to lmk how to query the database. Or, if they won't let me, figure out how to scrape info off CIS site.
 - Firefox
-	- Need to do all of it.
+	- All of it.
--- UT_Rated_Chrome/content.js
+++ UT_Rated_Chrome/content.js
@@ -1,3 +1,45 @@
+/*function makeXMLHttpRequest(url) {
+	var request = new XMLHttpRequest();
+	request.open("GET", url);
+	request.send();
+
+	if (request.status = 200)
+		return request.responseText;
+}*/
+
+function search (name, c_num) {
+	name = caseName(name);
+
+	/*console.log("Getting prof info for "+name+", and "+c_num[0]+" "+c_num[1]+".");
+	var request = makeXMLHttpRequest(baseURL+name);
+
+	if (request === 'undefined')
+		return "bad search request";
+
+	console.log("\""+request+"\"");
+
+	return request;*/
+
+	return "<a href=\""+baseURL+name+"\">"
+		+ "Teacher: " + name
+		+ ", Course: " + c_num[0]+"-"+c_num[1]+"</a>";
+}
+
+function getCourseNum(c_num) {
+	if (c_num.charAt(1) == ' ' && !isNaN(c_num.charAt(2))) // "A 123" or "A 123K"
+		return [c_num.charAt(0), c_num.substr(3, 4).trim()];
+	if (c_num.charAt(2) == ' ') // "AB 123" or "AB 123K"
+		return [c_num.substr(0, 2), c_num.substr(4, 4).trim()];
+	return [c_num.substr(0, 3), c_num.substr(5, 4).trim()]; // "ABC 123", "A B 123", "ABC 123K", or "A B 123K"
+}
+
+function caseName(name) {
+	return name.charAt(0)
+		+ name.substr(1, name.length-2).toLowerCase()
+		+ name.charAt(name.length-1);
+}
+
+var baseURL = "https://utdirect.utexas.edu/ctl/ecis/results/search.WBX?s_in_search_name=";
 var tbl = document.getElementsByClassName("rwd-table")[0];
 
 // Insert "Rating" to thead
@@ -9,15 +51,14 @@ ratingHead.innerHTML = "Rating"; // TODO add in a question field that opens a wi
 
 // Add cells to body for ratings
 var trs = tbl.tBodies[0].children;
-var c_num = "tmp"; // temp val
+var c_num = []; // [Field, Number]
 for (i = 0; i < trs.length; i++) {
 	if (trs[i].children[0].className == "course_header") { // header
-		c_num = trs[i].children[0].innerHTML.substr(4,8); // gets the course number
+		c_num = getCourseNum(trs[i].children[0].innerHTML.substr(4)); // gets the course number
 		continue;
 	}
 	var tds = trs[i].children;
 	var newtd = document.createElement("td");
 	trs[i].insertBefore(newtd, trs[i].children[5]);
-	newtd.innerHTML = c_num;
+	newtd.innerHTML = search(trs[i].children[4].innerHTML, c_num);
 }
-//http://www.ratemyprofessors.com/search.jsp?queryBy=teacherName&queryoption=HEADER&facetSearch=true&query=gheith&sid=1255&dept=Computer%20Science
